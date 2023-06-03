22e91c22c90683c2121c20d263145d4317ebf947
ananthomprakash <ananthomprakash@gmail.com>
2023-06-03 21:37:26 +0530

    Added CouponAPI endpoint and automapper, updated the program.cs file

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbcontext _db;
+
+        private ResponseDto _response;
+
+        private IMapper _mapper;
+
+
+        public CouponAPIController(AppDbcontext db,IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get Coupon
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result= _mapper.Map<IEnumerable<CouponDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess= false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        /// <summary>
+        /// Get Coupon by ID
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u=>u.CouponId==id);
+
+                _response.Result= _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
